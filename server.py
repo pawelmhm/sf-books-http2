75fbb4cd4830b55ccde594dac6938c71e9c16280
Pawel Miech <pawelmhm@gmail.com>
2016-07-27 12:51:47 +0200

    small cleanup

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 import json
 import sys
 
-from OpenSSL import SSL
-from twisted.internet.ssl import DefaultOpenSSLContextFactory
 from twisted.web import server
 from twisted.web.resource import Resource
 from twisted.internet import reactor
 from twisted.python import log
+
+from twisted.internet import endpoints
 
 def load_stock():
     with open("books.json") as stock_file:
@@ -33,28 +33,11 @@ class Book(Resource):
 
 if __name__ == "__main__":
     log.startLogging(sys.stdout)
-
     root = Resource()
     root.putChild(b"", Index())
     root.putChild(b"book", Book())
     site = server.Site(root)
-
-
-    class MyContextFactory(DefaultOpenSSLContextFactory):
-        # Context subclass to allow manipulating some context settings (e.g. cipher selection).
-        def cacheContext(self):
-            if self._context is None:
-                ctx = self._contextFactory(self.sslmethod)
-                ctx.set_options(SSL.OP_NO_SSLv2)
-                ctx.use_certificate_file(self.certificateFileName)
-                ctx.use_privatekey_file(self.privateKeyFileName)
-                # will this help?
-                #ctx.set_cipher_list("ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256")
-                # ctx.set_cipher_list("ECDH+AESGCM:DH+AESGCM:ECDH+AES256:DH+AES256:ECDH+AES128:DH+AES:ECDH+3DES:DH+3DES:RSA+AESGCM:RSA+AES:RSA+3DES:!aNULL:!MD5:!DSS")
-                self._context = ctx
-
-    # https://github.com/icing/mod_h2/issues/25
-    # ERR_SPDY_INADEQUATE_TRANSPORT_SECURITY
-    context_factory = MyContextFactory("key.pem", "cert.pem")
-    reactor.listenSSL(8080, site, context_factory)
+    endpoint_spec = "ssl:port=8080:privateKey=key.pem:extraCertChain=cert.pem"
+    server = endpoints.serverFromString(reactor, endpoint_spec)
+    server.listen(site)
     reactor.run()
